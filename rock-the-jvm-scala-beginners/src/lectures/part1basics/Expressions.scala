9b9525beae6d6173acc9addf2d186cbe71d4c6b1
cguem001 <charlyguemes@gmail.com>
2021-09-15 10:03:10 +0100

    variables and expressions done

--- rock-the-jvm-scala-beginners/src/lectures/part1basics/Expressions.scala
+++ rock-the-jvm-scala-beginners/src/lectures/part1basics/Expressions.scala
@@ -19,6 +19,54 @@ object Expressions extends App {
   aVariable += 3 // also works with -= *= /= ...... side effects
   println(aVariable)
 
-  // Instructions vs Expressions
+  // Instructions (DO) vs Expressions (VALUE)
+
+  // IF expression
+  val aCondition = true
+  val aConditionedValue = if (aCondition) 5 else 3 //IF EXPRESSION
+  println(aConditionedValue)
+  println(if (aCondition) 5 else 3)
+  println(1 + 3)
+
+  var i = 0
+  while (i < 10) {
+    println(i)
+    i += 1
+  }
+  // NEVER WRITE THIS AGAIN
+
+  // EVERYTHING in Scala is an Expression!
+
+  val aWeirdValue = (aVariable = 3) // Unit === void
+  println(aWeirdValue)
+
+  // side effects: println(), whiles, reassigning
+
+  // Code blocks
+
+  val aCodeBlock = {
+    val y = 2
+    val z = y + 1
+
+    if (z > 3) "hello" else "goodbye"
+  }
+
+  // 1. difference between "hello world" vs println ("hello world")?
+  // String "hello world" is a value of type string while println ("hello world") is an expression which is a side effect, it is a unit
+
+  //2. What's the value of the next expressions
+
+  val someValue = {
+    2 < 3
+  }
+  println(someValue)
+  // someValue = true someValue is a boolean
+
+  val someOtherValue = {
+    if (someValue) 239 else 986 // this expression is irrelevant
+    42
+  }
+  println(someOtherValue)
+  // someOtherValue = 42 is a value of type int
 
 }
